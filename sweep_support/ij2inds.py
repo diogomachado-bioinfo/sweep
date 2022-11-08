9f8f41d3cbfb84062c72c9bc77dbd9813efda04c
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2022-11-08 02:54:19 -0300

    Delete sweep_support directory

--- sweep_support/ij2inds.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-def ij2inds(ijs, tmcol):
-    return ((ijs[:,1] - 1) * tmcol) + (ijs[:,0])
--- sweep_support/seq2list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import numpy as np
-import more_itertools as mit
-def seq2list(sdna, q):
-    sdna=list(sdna)
-    b = np.array(sdna)
-    b = np.array(list(mit.windowed(b.ravel(), n=q)))
-    return np.array(b)
