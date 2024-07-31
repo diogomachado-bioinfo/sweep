53d24a72b0c21fe92a1f13db25cec6d19d9b2920
Diogo de Jesus Soares Machado <41521166+diogomachado-bioinfo@users.noreply.github.com>
2024-07-30 22:38:17 -0300

    Update orthbase.py
    Addition of the dtype parameter in the orthbase function.

--- sweep/orthbase.py
+++ sweep/orthbase.py
@@ -4,25 +4,27 @@
 from .sweep_support.orth import orth
 import numpy as np
 
-def orthbase(lin, col, seed=None):
+def orthbase(lin, col, seed=None, dtype=np.float32):
     """
     Generate an orthogonal basis matrix.
 
     Args:
-        lin (int): Number of rows in the matrix.
-        col (int): Number of columns in the matrix.
-        seed (int, optional): Seed for the random number generator.
+        - lin (int): Number of rows in the matrix.
+        - col (int): Number of columns in the matrix.
+        - seed (int, optional): Seed for the random number generator.
+        - dtype (data-type, optional): Desired data-type for the array,
+          default is np.float32.
 
     Returns:
-        ndarray: Orthogonal basis matrix.
+        - ndarray: Orthogonal basis matrix.
     """
     if seed is not None:
         np.random.seed(seed)
 
     if lin != col:
-        Ro = orth(np.random.rand(lin, col+1))
+        Ro = orth(np.random.rand(lin, col + 1).astype(dtype))
         mret = Ro[:, 1:]
     else:
-        mret = orth(np.random.rand(lin, col))
+        mret = orth(np.random.rand(lin, col).astype(dtype))
 
-    return np.array(mret)
+    return np.array(mret, dtype=dtype)
